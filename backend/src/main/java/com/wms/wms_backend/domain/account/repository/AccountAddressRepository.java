f462b6e282ad406a14d90d7bceacaed0f468355a
jeongbin5211 <hjn123493@gmail.com>
2026-06-14 04:52:13 +0900

    [fix] 거래처 주소 조회 지연 로딩 오류 수정

--- backend/src/main/java/com/wms/wms_backend/domain/account/repository/AccountAddressRepository.java
+++ backend/src/main/java/com/wms/wms_backend/domain/account/repository/AccountAddressRepository.java
@@ -1,6 +1,7 @@
 package com.wms.wms_backend.domain.account.repository;
 
 import com.wms.wms_backend.domain.account.entity.AccountAddress;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +11,6 @@ public interface AccountAddressRepository extends JpaRepository<AccountAddress, 
 
     Optional<AccountAddress> findByAddressCode(String addressCode);
 
+    @EntityGraph(attributePaths = "account")
     List<AccountAddress> findByTopAccountIdAndUseYnOrderByIdAsc(Long topAccountId, String useYn);
 }
